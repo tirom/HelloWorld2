aa1ae7be10554723e872589e285726864ce67d43
nhuthanhphong@gmail.com <nhuthanhphong@gmail.com>
2018-10-11 10:25:30 +0700

    PollyHttpClient.csproj

---
+++ kikker/src/PollyWithHttpClient/Controllers/MyController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PollyWithHttpClient.Controllers
+{
+	[Route("api/my")]
+	public class MyController : Controller
+    {
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly MyGitHubClient _gitHubClient;
+
+		public MyController(IHttpClientFactory httpClientFactory, MyGitHubClient gitGitHubClient)
+		{
+			_httpClientFactory = httpClientFactory;
+			_gitHubClient = gitGitHubClient;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> SomeActionAsync()
+		{
+			// Get an HttpClient configured to the specification you defined in StartUp.
+			//var client = _httpClientFactory.CreateClient("TestGitHub");
+			//var result = await client.GetStringAsync("/");
+			//return Ok(result);
+			//return Ok(await client.GetStringAsync("/user"));
+
+			var result = await _gitHubClient.Client.GetStringAsync("/");
+			return Ok(result);
+		}
+		
+    }
+}
---
+++ kikker/src/PollyWithHttpClient/Startup.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Extensions.Http;
+using Polly.Timeout;
+using PollyWithHttpClient.Handlers;
+
+namespace PollyWithHttpClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+			var timeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(10)); // Timeout for an individual try
+			var noOpPolicy = Policy.NoOpAsync().AsAsyncPolicy<HttpResponseMessage>();
+
+			services.AddTransient<TimingHandler>();
+			services.AddTransient<ValidateHeaderHandler>();
+			services.AddHttpClient<MyGitHubClient>(client =>
+			{
+				client.BaseAddress = new Uri("https://api.github.com/");
+				client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+				client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+			})
+			.AddPolicyHandler(timeoutPolicy)
+			.AddHttpMessageHandler<TimingHandler>() // This handler is on the outside and executes first on the way out and last on the way in.
+			.AddHttpMessageHandler<ValidateHeaderHandler>(); // This handler is on the inside, closest to the request.
+			
+
+			var registry = services.AddPolicyRegistry();
+			//registry.Add("defaultretrystrategy",
+			//	HttpPolicyExtensions.HandleTransientHttpError().WaitAndRetryAsync(
+			//		new[]
+			//		{
+			//			TimeSpan.FromSeconds(1),
+			//			TimeSpan.FromSeconds(5),
+			//			TimeSpan.FromSeconds(10)
+			//		}
+			//		));
+			registry.Add("defaultcircuitbreaker",
+				HttpPolicyExtensions.HandleTransientHttpError().CircuitBreakerAsync(
+					handledEventsAllowedBeforeBreaking: 3,
+					durationOfBreak: TimeSpan.FromSeconds(30)
+					));						
+			
+			var retryPolicy = HttpPolicyExtensions
+				.HandleTransientHttpError()//Network failures (System.Net.Http.HttpRequestException),HTTP 5XX status codes (server errors),HTTP 408 status code (request timeout)
+				.Or<TimeoutRejectedException>() // thrown by Polly's TimeoutPolicy if the inner call times out
+				.WaitAndRetryAsync(new[]
+				{
+						TimeSpan.FromSeconds(1),
+						TimeSpan.FromSeconds(5),
+						TimeSpan.FromSeconds(10)
+				});
+			
+			// Configure a client named as "GitHub", with various default properties.
+			services.AddHttpClient("TestGitHub", client =>
+			{
+				client.BaseAddress = new Uri("https://api.github.com/");
+				client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+				client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactoryTesting");
+				client.Timeout = TimeSpan.FromSeconds(60); // Overall timeout across all tries
+			})
+			.AddPolicyHandler(request => request.Method == HttpMethod.Get ? retryPolicy : noOpPolicy)
+			//.AddPolicyHandlerFromRegistry("defaultretrystrategy")
+			.AddPolicyHandlerFromRegistry("defaultcircuitbreaker")	//already registered above		
+			//.AddTransientHttpErrorPolicy(builder => builder.CircuitBreakerAsync(
+			//	handledEventsAllowedBeforeBreaking: 3,
+			//	durationOfBreak: TimeSpan.FromSeconds(30)
+			//))			
+			.AddPolicyHandler(timeoutPolicy);
+
+			services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
